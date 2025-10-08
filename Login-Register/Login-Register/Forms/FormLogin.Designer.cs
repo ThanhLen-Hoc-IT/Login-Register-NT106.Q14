582c82f418b0a13e0ac34bfb2798165c956cc323
HuynhMinhTri <trihuynhminh2006@gmail.com>
2025-10-09 00:46:28 +0700

    sign up

--- Login-Register/Login-Register/Forms/FormLogin.Designer.cs
+++ Login-Register/Login-Register/Forms/FormLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Login_Register.Forms
 {
-    partial class FormLogin
+    partial class s
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TenDangNhap = new System.Windows.Forms.TextBox();
+            this.EmailDangNhap = new System.Windows.Forms.TextBox();
+            this.MatKhau = new System.Windows.Forms.TextBox();
+            this.XacNhanMatkhau = new System.Windows.Forms.TextBox();
+            this.Dangky = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TenDangNhap
+            // 
+            this.TenDangNhap.Location = new System.Drawing.Point(165, 96);
+            this.TenDangNhap.Name = "TenDangNhap";
+            this.TenDangNhap.Size = new System.Drawing.Size(100, 20);
+            this.TenDangNhap.TabIndex = 0;
+            // 
+            // EmailDangNhap
+            // 
+            this.EmailDangNhap.Location = new System.Drawing.Point(171, 138);
+            this.EmailDangNhap.Name = "EmailDangNhap";
+            this.EmailDangNhap.Size = new System.Drawing.Size(100, 20);
+            this.EmailDangNhap.TabIndex = 1;
+            // 
+            // MatKhau
+            // 
+            this.MatKhau.Location = new System.Drawing.Point(171, 175);
+            this.MatKhau.Name = "MatKhau";
+            this.MatKhau.Size = new System.Drawing.Size(100, 20);
+            this.MatKhau.TabIndex = 2;
+            // 
+            // XacNhanMatkhau
+            // 
+            this.XacNhanMatkhau.Location = new System.Drawing.Point(171, 213);
+            this.XacNhanMatkhau.Name = "XacNhanMatkhau";
+            this.XacNhanMatkhau.Size = new System.Drawing.Size(100, 20);
+            this.XacNhanMatkhau.TabIndex = 3;
+            // 
+            // Dangky
+            // 
+            this.Dangky.Location = new System.Drawing.Point(186, 268);
+            this.Dangky.Name = "Dangky";
+            this.Dangky.Size = new System.Drawing.Size(75, 23);
+            this.Dangky.TabIndex = 4;
+            this.Dangky.Text = "dang ky";
+            this.Dangky.UseVisualStyleBackColor = true;
+            this.Dangky.Click += new System.EventHandler(this.Dangky_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ten";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mat Khau";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Xac Nhan Mat khau";
+            // 
+            // s
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Dangky);
+            this.Controls.Add(this.XacNhanMatkhau);
+            this.Controls.Add(this.MatKhau);
+            this.Controls.Add(this.EmailDangNhap);
+            this.Controls.Add(this.TenDangNhap);
+            this.Name = "s";
             this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TenDangNhap;
+        private System.Windows.Forms.TextBox EmailDangNhap;
+        private System.Windows.Forms.TextBox MatKhau;
+        private System.Windows.Forms.TextBox XacNhanMatkhau;
+        private System.Windows.Forms.Button Dangky;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Login-Register/Login-Register/Forms/FormLogin.cs
+++ Login-Register/Login-Register/Forms/FormLogin.cs
@@ -1,20 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Login_Register.Utils;
 
 namespace Login_Register.Forms
 {
-    public partial class FormLogin : Form
+    public partial class s : Form
     {
-        public FormLogin()
+        public s()
         {
             InitializeComponent();
         }
-    }
+
+        private void DangKy_Click(object sender, EventArgs e)
+        {
+            if (ValidationHelper.IsEmpty(TenDangNhap.Text))
+            {
+                MessageBox.Show("Ten dang nhap khong duoc de trong");
+            }
+            if (ValidationHelper.IsEmpty(EmailDangNhap.Text))
+            {
+                MessageBox.Show("Email khong duoc de trong");
+            }
+            if (ValidationHelper.IsEmpty(MatKhau.Text))
+            {
+                MessageBox.Show("Mat khau khong duoc de trong");
+            }
+            if (ValidationHelper.IsEmpty(XacNhanMatkhau.Text))
+            {
+                MessageBox.Show("Xac nhan mat khau khong duoc de trong");
+            }
+            if (!ValidationHelper.IsValidEmail(EmailDangNhap.Text))
+            {
+                MessageBox.Show("Email khong hop le");
+            }
+            if (!ValidationHelper.IsValidPassword(MatKhau.Text))
+            {
+                MessageBox.Show("Mat khau phai co it nhat 6 ky tu");
+            }
+            if (!ValidationHelper.IsPasswordMatch(MatKhau.Text, XacNhanMatkhau.Text))
+            {
+                MessageBox.Show("Mat khau va xac nhan mat khau khong trung khop");
+            }
+
+            // --- Sau khi kiểm tra hợp lệ ---
+            string connStr = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO Users (Username, Email, Password) VALUES (@Username, @Email, @Password)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    string Ten = TenDangNhap.Text;
+                    string Email = EmailDangNhap.Text;
+                    string MatKhauStr = MatKhau.Text;
+                    cmd.Parameters.AddWithValue("@Username", Ten);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@Password", MatKhauStr); // sau này sẽ hash
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                    if (rows > 0)
+                    {
+                        MessageBox.Show("Đăng ký thành công!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lỗi khi lưu vào database!");
+                    }
+                }
+            }
+        }
 }
